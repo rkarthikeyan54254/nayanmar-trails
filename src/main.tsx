8f3fdeabf609c907332e6d89cd495fb6b031df1c
Rama Karthikeyan <rkarthikeyan54254@gmail.com>
2026-09-23 13:33:22 +0530

    Wrap v1 app in production error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './styles.css';
 import App from './App';
+import { ErrorBoundary } from './ErrorBoundary';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>,
+  <React.StrictMode><ErrorBoundary><App /></ErrorBoundary></React.StrictMode>,
 );
