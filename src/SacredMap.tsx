d285c86e0a14051250e74f5bf199028e78a20305
Rama Karthikeyan <rkarthikeyan54254@gmail.com>
2026-09-24 09:21:23 +0530

    Use richer gopuram markers on sacred map

--- src/SacredMap.tsx
+++ src/SacredMap.tsx
@@ -27,15 +27,29 @@ export type CoveragePoint = {
 };
 
 const gopuramMarkup = `
-<svg viewBox="0 0 64 64" aria-hidden="true">
-  <path d="M29 3h6l2 5H27l2-5Z"/>
-  <path d="M24 10h16l3 7H21l3-7Z"/>
-  <path d="M19 19h26l3 8H16l3-8Z"/>
-  <path d="M14 29h36l3 10H11l3-10Z"/>
-  <path d="M9 41h46l3 9H6l3-9Z"/>
-  <path d="M4 52h56v8H4z"/>
-  <path class="door" d="M25 39h14v21H25z"/>
-</svg>`;
+<svg viewBox="0 0 64 72" aria-hidden="true">
+  <circle class="finial" cx="32" cy="3.5" r="2.25"/>
+  <path class="finial" d="M29.2 6h5.6l2.2 4H27z"/>
+  <path class="tier" d="M25.5 11h13l3.2 6H22.3z"/>
+  <path class="band" d="M21.2 18h21.6l2.6 2.8H18.6z"/>
+  <path class="tier" d="M19 22h26l3.7 7.2H15.3z"/>
+  <path class="band" d="M14 30.3h36l2.6 3.1H11.4z"/>
+  <path class="tier" d="M12.5 35h39l4.1 8.3H8.4z"/>
+  <path class="band" d="M7 44.7h50l2.4 3.3H4.6z"/>
+  <path class="tier" d="M6.4 49.6h51.2L61 58H3z"/>
+  <path class="base" d="M2.5 59.7h59v8.8h-59z"/>
+  <path class="arch" d="M23.6 49.2c.4-6.2 3.2-9.2 8.4-9.2s8 3 8.4 9.2h-3.2c-.6-4-2.3-6-5.2-6s-4.6 2-5.2 6z"/>
+  <path class="door" d="M25.7 48.8h12.6v19.7H25.7z"/>
+  <g class="niches">
+    <rect x="21.8" y="24.8" width="3.5" height="3" rx=".6"/>
+    <rect x="30.2" y="24.8" width="3.5" height="3" rx=".6"/>
+    <rect x="38.7" y="24.8" width="3.5" height="3" rx=".6"/>
+    <rect x="15.8" y="38" width="3.7" height="3.2" rx=".6"/>
+    <rect x="23.7" y="38" width="3.7" height="3.2" rx=".6"/>
+    <rect x="36.6" y="38" width="3.7" height="3.2" rx=".6"/>
+    <rect x="44.5" y="38" width="3.7" height="3.2" rx=".6"/>
+  </g>
+</svg>`
 
 const HERITAGE_MAP_STYLE: StyleSpecification = {
   version: 8,
